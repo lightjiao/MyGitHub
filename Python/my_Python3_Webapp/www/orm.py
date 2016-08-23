ce3dd17a676741635d5f04352d8c569b69ddc1ff
焦鸿彬 <bhjnk945766721@qq.com>
2016-08-23 11:22:51 +0800

    彻底完善orm

--- Python/my_Python3_Webapp/www/orm.py
+++ Python/my_Python3_Webapp/www/orm.py
@@ -1,70 +1,110 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging; logging.basicConfig(level=logging.INFO)
-from logging import log
+__author__ = 'Lights Jiao'
 
-import asyncio, os, json, time
-from datetime import datetime
+import logging, asyncio, aiomysql
 
-from aiohttp import web
-
-import aiomysql
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
 
 
 #创建连接池
-@asyncio.coroutine
-def create_pool(loop, **kw):
+async def create_pool(loop, **kw):
     logging.info('create database connection pool ...')
     global __pool
-    __pool = yield from aiomysql.create_pool(
-        host       = kw.get('host',      'localhost'),
-        port       = kw.get('port',      3306),
-        user       = kw['user'],
-        password   = kw['password'],
-        db         = kw['db'],
-        charset    = kw.get('charset',   'utf8'),
-        autocommit = kw.get('autocommit', True),
-        maxsize    = kw.get('maxsize',   10),
-        minsize    = kw.get('minsize',   1),
-        loop       = loop
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
     )
 
 
 #Select 语句
-@asyncio.coroutine
-def select(sql, args, size = None):
+async def select(sql, args, size = None):
     log(sql, args)
     global __pool
-    with (yield from __pool) as conn:
-        cur = yield from conn.cursor(aiomysql.DictCursor)
-        yield from cur.execute(sql.replace('?', '%s'), args or ())
-        if size:
-            rs = yield from cur.fetchmany(size)
-        else:
-            rs = yield from cur.fetchall()
-        yield from cur.close()
-        logging.info('row returned: %s' % len(rs))
-        return rs
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+            logging.info('row returned: %s' % len(rs))
+            return rs
 
 #Insert, Update, Delete
 #要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数，因为这3种SQL的执行都需要相同的参数，以及返回一个整数表示影响的行数：
-@asyncio.coroutine
-def execute(sql, args):
+async def execute(sql, args, autocommit=True):
     log(sql) # log(sql, args)
-    with (yield from __pool) as conn:
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = yield from conn.cursor()
-            yield from cur.execute(sql.replace('?', '%s'), args)
-            affected = cur.rowcount
-            yield from cur.close()
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
+            if not autocommit:  ## 这里有一些疑问
+                await conn.rollback()
             raise
         return affected
 
 
-
 ###################### Model Begin #######################
+## what is this
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name        = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default     = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name = None, primary_key = False, default = None, ddl = 'varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+    def __init__(self, name = None, primary_key = False, default = False, ddl = 'boolean'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class IntegerField(Field):
+    def __init__(self, name = None, primary_key = False, default = 0, ddl = 'bigint'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class FloatField(Field):
+    def __init__(self, name = None, primary_key = False, default = 0.0, ddl = 'real'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class TextField(Field):
+    def __init__(self, name = None, primary_key = False, default = None, ddl = 'text'):
+        super().__init__(name, ddl, primary_key, default)
+
 
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
@@ -77,9 +117,9 @@ class ModelMetaclass(type):
         logging.info('found model: %s (table: %s)' % (name, tableName))
 
         #获取所有的Field 和主键名
-        mappings   = dict()
-        fields     = []
-        primaryKey = None
+        mappings   = dict() #保存属性和列的映射关系
+        fields     = []     #除主键外的属性名
+        primaryKey = None   #主键属性名
         for k, v in attrs.items():
             if isinstance(v, Field):
                 logging.info('    found mappings: %s ==> %s' % (k ,v))
@@ -87,14 +127,14 @@ class ModelMetaclass(type):
                 #找到主键
                 if v.primary_key:
                     if primaryKey:
-                        raise RuntimeError('Duplicate primary key for field: %s' % k)
+                        raise StandardError('Duplicate primary key for field: %s' % k)
                     primaryKey = k
 
                 else:
                     fields.append(k)
 
         if not primaryKey:
-            raise RuntimeError('Primary key not found.')
+            raise StandardError('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
 
@@ -111,12 +151,6 @@ class ModelMetaclass(type):
         attrs['__delete__'] = 'delete from `%s` where `%s` = ?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
-## what is this
-def create_args_string(num):
-    L = []
-    for n in range(num):
-        L.append('?')
-    return ', '.join(L)
 
 class Model(dict, metaclass = ModelMetaclass):
     def __init__(self, **kw):
@@ -144,53 +178,76 @@ class Model(dict, metaclass = ModelMetaclass):
                 setattr(self, key, value)
         return value
 
+
     @classmethod
-    @asyncio.coroutine
-    def find(cls, pk):
+    async def findAll(cls, where = None, args = None, **kw):
+        ' find object by where clause. '
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+
+        orderBy = kw.get('orderby', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?,?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    async def findNumber(cls, selectField, where = None, args = None):
+        ' find number by select and where. '
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
+
+
+    @classmethod
+    async def find(cls, pk):
         '  find object by primary key. '
-        rs = yield from select('%s where `%s` = ?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        rs = await select('%s where `%s` = ?' % (cls.__select__, cls.__primary_key__), [pk], 1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
 
-    @asyncio.coroutine
-    def save(self):
-        args = list(map(self.getValueOrDefault(), self.__fields__))
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primary_key__))
-        rows = yield from execute(self.__insert__, args)
+        rows = await execute(self.__insert__, args)
         if rows != 1:
-            logging.warning('faild to insert record: affected rows: %s' % rows) #logging.warn was deprecate
+            logging.warning('failed to insert record: affected rows: %s' % rows) #logging.warn was deprecate
 
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warning('failed to update by primary key: affected rows: %s' % rows)
 
-class Field(object):
-    def __init__(self, name, column_type, primary_key, default):
-        self.name        = name
-        self.column_type = column_type
-        self.primary_key = primary_key
-        self.default     = default
-
-    def __str__(self):
-        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-
-class StringField(Field):
-    def __init__(self, name = None, primary_key = False, default = None, ddl = 'varchar(100)'):
-        super().__init__(name, ddl, primary_key, default)
-
-
-########################### write by myself ###########################
-class IntegerField(Field):
-    def __init__(self, name = None, primary_key = False, default = None, ddl = 'integer'):
-        super().__init__(name, ddl, primary_key, default)
-
-class BooleanField(Field):
-    def __init__(self, name = None, primary_key = False, default = None, ddl = 'bool'):
-        super().__init__(name, ddl, primary_key, default)
-
-class FloatField(Field):
-    def __init__(self, name = None, primary_key = False, default = None, ddl = 'float'):
-        super().__init__(name, ddl, primary_key, default)
-
-class TextField(Field):
-    def __init__(self, name = None, primary_key = False, default = None, ddl = 'text'):#????
-        super().__init__(name, ddl, primary_key, default)
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warning('failed to delete by primary key: affected rows: %s' % rows)
+            
