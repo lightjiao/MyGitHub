2e2ee4c8518de5d213ee61bb672a2bb1fcee4d86
焦鸿彬 <bhjnk945766721@qq.com>
2016-08-23 14:20:28 +0800

    compelete the test and success

--- Python/my_Python3_Webapp/www/test.py
+++ Python/my_Python3_Webapp/www/test.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import asyncio
+
 import orm
 from models import User, Blog, Comment
 
-def Modeltest():
-    yield from orm.create_pool(user = 'www-data', password = '1', database = 'mypython3webapp')
+async def Modeltest(loop):
+    await orm.create_pool(loop, user = 'www-data', password = '1', db = 'mypython3webapp')
 
     u = User(name = 'test', email = 'test@example.com', passwd = '123456', image = 'about:blank')
 
-    yield from u.save()
+    await u.save()
 
 if __name__ == '__main__':
-    for x in Modeltest():
-        pass
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(Modeltest(loop))
+    loop.run_forever()
