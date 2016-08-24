a29f8140e09bb3c69bac07aa69dfd748b55e3416
焦鸿彬 <bhjnk945766721@qq.com>
2016-08-24 15:26:08 +0800

    提交配置文件

---
+++ Python/myPython3WebApp/conf/config_override.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Lights Jiao'
+
+'''
+Override Configuration
+'''
+
+configs = {
+    'db':{
+        'host':'127.0.0.1'
+    }
+}
--- Python/myPython3WebApp/www/handlers.py
+++ Python/myPython3WebApp/www/handlers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'Michael Liao'
+__author__ = 'Lights Jiao'
 
 ' url handlers '
