b396eddf6d4a769a62da27b31202ba2a9e251edc
焦鸿彬 <bhjnk945766721@qq.com>
2016-09-05 14:47:46 +0800

    仍然没能解决split函数在极限情况下的处理; 添加了一个VS的插件格式化了一下代码; 删除了一些以前留下的测试代码

--- C_PlusPlus/Console/Console/main.cpp
+++ C_PlusPlus/Console/Console/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "stdio.h"
 #include "stdint.h"
-//#include "winsock.h"//htons ntohs
-//#include "winsock2.h"//htons ntohs
 #include "vector"
 #include "time.h"
 #include "get100.h"
@@ -20,20 +18,21 @@
 using namespace std;
 
 void func_1()
-{   // 小字节序：0  44  大字节序：1023  44
+{
+    // 小字节序：0  44  大字节序：1023  44
     int a[10];
     int  i = 0;
+    
     for (i; i < 10; i++)
     {
-        *(a + i) = i+1014;
+        *(a + i) = i + 1014;
     }
+    
     short *p = (short*)(&a + 1);//此处会跳过整个数组a  //a+1是内存地址以[数组元素的长度]加一， &a+1是内存地址[以整个数组的长度]加一
     short *q = p - 1;
-
     char c = 300;
     char* d = &c - 1;
-    printf("%d %d\n", *q, c);           //大字节序：24,_      小字节序：10,_(0, 44)
-
+    printf("%d %d\n", *q, c);   //大字节序：24,_      小字节序：10,_(0, 44)
     return;
 }
 
@@ -46,17 +45,14 @@ void func_2()
     int (*y)[3][3] = &a;
     int (*q)[3]    = &a[0];
     int *z         = &a[0][0];
-
-    printf("%d\n", (*(x + 1))[0]);    // 4      
+    printf("%d\n", (*(x + 1))[0]);    // 4
     printf("%d\n", *(k + 1));         // 2
     printf("%d\n", (*(*(y + 1)))[0]); // _____?
     printf("%d\n", (*(q + 1))[0]);    // 4
     printf("%d\n", *(z + 1));         // 2
-
-    int *p = (int *)(y + 1);          
+    int *p = (int *)(y + 1);
     printf("%d\n", *(p - 1));         // 9
-
-    return;                           //good, answer is right!
+    return;                           // good, answer is right!
 }
 
 void test_func_1()
@@ -68,206 +64,98 @@ void test_func_1()
     printf("&a=%x\n&p=%x\n", &a, p);
     printf("a[0] = %d\n", a[0]);
     printf("  *p = %d\n", *p);
-    printf("sizeof(a) = %d\n",sizeof(a));
+    printf("sizeof(a) = %d\n", sizeof(a));
     printf("sizeof(short)=%d\nsizeof(int)=%d\n", sizeof(short), sizeof(int));
-
     return;
 }
-
-class testPrint
-{
-public:
-    testPrint(unsigned int n)
-    {
-        printf("%d:construct!\n",n);
-    };
-
-    ~testPrint()
-    {
-        printf("release!\n");
-    };
-
-    void testPrint::ThePrintFunc(unsigned int n)
-    {
-        printf("%d\n", n);
-    }
-};
 
 void TrueSwamp(int& a, int& b)
 {
     int c = a;
     a = b;
     b = c;
-
     return;
-}
-
-
-
-void PrintMultiTable()
-{
-    int i, j = 0;
-    for (i = 1; i <= 9;i++)
-    {
-        for (j = 1; j <= i;j++)
-        {
-            printf("%d * %d = %d   ", j, i, i*j);
-            if (i*j<10)
-            {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
 }
 
 typedef uint32_t uint_32_t;
 
-//what is this
+//通过位移，快速的计算开根号操作，但结果不够精确
 float fast_sqrt(float x)
 {
     uint_32_t x_bits = 0;
-    
     x_bits = *((uint_32_t*) &x);
-    
     x_bits = (x_bits >> 1) + 532369198;
-
     return *((float*)&x_bits);
-}
-
-//测试vector的 erase方法
-void vector_test()
-{
-    vector<int> vecInt;
-    for (int i = 0; i < 10; i++)
-    {
-        vecInt.push_back(i);
-    }
-
-    printf("\n\n***********************\n");
-    vector<int>::iterator it = vecInt.begin();
-    for (; it != vecInt.end();)
-    {
-        printf("%d\n", *it);
-        it++;
-    }
-
-    printf("\n\n***********************\n");
-    it = vecInt.begin();
-    for (; it != vecInt.end();)
-    {
-        printf("%d\n", *it);
-        it = vecInt.erase(it);
-    }
-    printf("hello");
-    for (; it != vecInt.end();)
-    {
-        printf("%d\n", *it);
-        it = vecInt.erase(it++);
-    }
-
-    vecInt.clear();
-    for (int i = 0; i < 10; i++)
-    {
-        vecInt.push_back(i);
-    }
-
-    printf("\n\n***********************\n");
-    it = vecInt.begin();
-    for (; it != vecInt.end();)
-    {
-        printf("%d\n", *it);
-        it = vecInt.erase(it++);
-    }
-    printf("hello");
-    for (; it != vecInt.end();)
-    {
-        printf("%d\n", *it);
-        it = vecInt.erase(it++);
-    }
-
-    return;
 }
 
 
 //此splite函数无法区分一种特殊情况, 字符串为 "弢|你好啊|123|弢弢弢|abc" 按照 "|" 为分隔符时 会出错，原因是 汉字 "弢" 与分隔符 "|" 有相同的ACSII码
+//C++的string 类 find以及C 语言本身的 strtok方法都无法区分这种特殊情况
 int strsplit(const char *str, char *parts[], const char *delimiter)
 {
     char *pch;
     int i = 0;
     char *copy = NULL, *tmp = NULL;
-
     copy = strdup(str);
+    
     if (!copy)
+    {
         goto bad;
-
+    }
+    
     pch = strtok(copy, delimiter);
-
     tmp = strdup(pch);
+    
     if (!tmp)
+    {
         goto bad;
-
+    }
+    
     parts[i++] = tmp;
-
-    while (pch) {
+    
+    while (pch)
+    {
         pch = strtok(NULL, delimiter);
-        if (NULL == pch) break;
-
+        
+        if (NULL == pch) { break; }
+        
         tmp = strdup(pch);
+        
         if (!tmp)
-            goto bad;
-
+        { goto bad; }
+        
         parts[i++] = tmp;
     }
-
+    
     free(copy);
     return i;
-
 bad:
     free(copy);
+    
     for (int j = 0; j < i; j++)
-        free(parts[j]);
+    { free(parts[j]); }
+    
     return -1;
 }
 
 //测试splite函数， 能够按照汉字为分隔符进行分割，但区分不了汉字 "弢" 与分隔符 "|"
 void test004()
 {
-    char str[] = "123弢啦啦弢abc弢";
+    char str[] = "123弢啦啦弢abc弢";//"弢|你好啊|123|弢弢弢|abc"
     char *parts[8];
-    size_t size = strsplit(str, parts, "弢");
+    size_t size = strsplit(str, parts, "弢"); //"|"
     int i = 0;
-    for (; i < size; ++i) {
-        printf("%s\n", parts[i]);
-    }
-}
-
-void test_strsplit_FromInternet()
-{
-    char* sTestString = "弢|你好啊|123|弢弢弢|abc";
-    char* parts[6] = { 0 };
-
-    int size = strsplit(sTestString, parts, "|");
-
-    if (size == -1)
-    {
-        printf("run error!\n");
-        return;
-    }
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%s\n", parts[i]);
-    }
-
-    return;
-}
-
-void main(void) {
-    //test_strsplit_FromInternet();
-    printf("弢\n");
-    char a;
-    cin >> a;
     
+    for (; i < size; ++i)
+    {
+        printf("%s\n", parts[i]);
+    }
+}
+
+
+void main(void)
+{
+    char __TEMP__;
+    cin >> __TEMP__;
     return;
 }
