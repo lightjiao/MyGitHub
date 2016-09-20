ea59e45042822184edf8ded086fbc200d071132e
Lights_Jiao <bhjnk945766721@qq.com>
2016-09-20 16:39:58 +0800

    增加了0-1背包问题的求解; 稍微整理了一下代码，将所有与字符串分割的代码都整理到StringWithMySplitFunc.h 文件中

--- C_PlusPlus/Console/main.cpp
+++ C_PlusPlus/Console/main.cpp
@@ -3,13 +3,15 @@
 #include "stdint.h"
 #include "vector"
 #include "time.h"
-#include "get100.h"
 #include <cmath>
 #include "string.h"
 #include "stdlib.h"
-#include "StringWithMySplitFunc.h"
-#include "charReader.h"
-#include "reserve_resize.h"
+
+#include "get100.h"                 //网传1, 2, ..., 9数字组合结果等于 100
+#include "StringWithMySplitFunc.h"  //split函数处理生僻汉字
+#include "charReader.h"             //没能实现的json解析器
+#include "reserve_resize.h"         //测试STL的reserve & resize 方法
+#include "knapstack.h"              //0-1 背包问题
 
 #pragma warning(push)
 #pragma warning(disable: 4996)
@@ -34,71 +36,6 @@ float fast_sqrt(float x)
 }
 
 
-//此splite函数无法区分一种特殊情况, 字符串为 "弢|你好啊|123|弢弢弢|abc" 按照 "|" 为分隔符时 会出错，原因是 汉字 "弢" 与分隔符 "|" 有相同的ACSII码
-//C++的string 类 find以及C 语言本身的 strtok方法都无法区分这种特殊情况
-int strsplit(const char *str, char *parts[], const char *delimiter)
-{
-    char *pch;
-    int i = 0;
-    char *copy = NULL, *tmp = NULL;
-    copy = strdup(str);
-    
-    if (!copy)
-    {
-        goto bad;
-    }
-    
-    pch = strtok(copy, delimiter);
-    tmp = strdup(pch);
-    
-    if (!tmp)
-    {
-        goto bad;
-    }
-    
-    parts[i++] = tmp;
-    
-    while (pch)
-    {
-        pch = strtok(NULL, delimiter);
-        
-        if (NULL == pch) { break; }
-        
-        tmp = strdup(pch);
-        
-        if (!tmp)
-        { goto bad; }
-        
-        parts[i++] = tmp;
-    }
-    
-    free(copy);
-    return i;
-bad:
-    free(copy);
-    
-    for (int j = 0; j < i; j++)
-    {
-        free(parts[j]);
-    }
-    
-    return -1;
-}
-//测试splite函数， 能够按照汉字为分隔符进行分割，但区分不了汉字 "弢" 与分隔符 "|"
-void test004()
-{
-    char str[] = "123弢啦啦弢abc弢";//"弢|你好啊|123|弢弢弢|abc"
-    char *parts[8];
-    size_t size = strsplit(str, parts, "弢"); //"|"
-    int i = 0;
-    
-    for (; i < size; ++i)
-    {
-        printf("%s\n", parts[i]);
-    }
-    
-    return;
-}
 
 //西山居面试题，递归+循环
 int cc(int i)
@@ -115,6 +52,9 @@ void testcc()
     return;
 }
 
+
+//lept_json 教程中 strtod的说明与验证
+/* http://en.cppreference.com/w/c/string/byte/strtof */
 #include <errno.h>
 void test_strtod()
 {
@@ -133,12 +73,9 @@ void test_strtod()
     }
 }
 
-void main(void)
+
+int main(void)
 {
-	//测试STL的reserve、resize方法
-	//test_Reserve_Resize_Main();
-
-    test_strtod();
-
-    return;
+    test_knapstack();
+    return 0;
 }
