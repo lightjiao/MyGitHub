d1fc4a5094c7927f0231f9458c211391ff6aa961
Lights_Jiao <bhjnk945766721@qq.com>
2016-09-21 15:33:40 +0800

    完成一个爬下整个网站的爬虫，但没有验证数据完整性，以及换一个'www.meizitu.com'的网站就爬不了，或许与每个网站的代码结构不一样有关系，后续再完善吧

--- Python/WebSpider/WebSpider_for_beauty.py
+++ Python/WebSpider/WebSpider_for_beauty.py
@@ -9,11 +9,11 @@ targetDir = r'E:/Picture/Python'
 
 mainweburl = weburl = 'http://www.tubaiwan.com' #这是一个黄图网站， 嗯
 
-re_Img = re.compile(r'(http:([^\"\$\;])*(jpg|png|gif|bmp))') #获取图片的正则表达式
+re_Img = re.compile(r'(http:[^\"\$\;]*(jpg|png|gif|bmp))') #获取图片的正则表达式
 
 re_Href = re.compile(r'(href\s?=\s?\"[^\"]*\")') #获取跳转链接的正则表达式
-re_GetLinkFromHref = re.compile(r'(\"[^\"]*\")') #从href 的结果中提取链接
-re_Tubaiwan = re.compile(r'www\.')
+re_GetLinkFromHref = re.compile(r'((http|\/)[^\"\$\;]*)') #从href 的结果中提取链接
+re_Tubaiwan = re.compile(r'\.*')
 #re_GetLinkFromHref.findall(n)[0][1:-1] #获取纯净的href链接
 
 def destFile(path):
@@ -24,11 +24,28 @@ def destFile(path):
     t = os.path.join(targetDir, path[pos+1:])
     return t
 
-
+#用于存储已经爬过的url 需要占用大量的内存，内存浪费多。
+allUrlSet = set()
 
 #从链接中获取新的链接
-def GetHrefFromUrl():
-    pass
+def GetHrefFromUrl(url):
+    webPage = urllib.request.urlopen(url)
+    try:
+        pageData = webPage.read().decode('utf-8')
+    except Exception as e:
+        pageData = webPage.read().decode('gbk')
+
+    for n in set(re_Href.findall(pageData)):
+        url = re_GetLinkFromHref.findall(n)[0][0]
+        if not url.startswith('http'):
+            tempurl = 'http://www.tubaiwan.com' + url
+            url = tempurl
+        if -1 != url.find('www.tubaiwan'):
+            print('获取跳转链接：' + url)
+            if url not in allUrlSet:
+                allUrlSet.add(url)
+                GetImgFromUrl(url)
+
 
 
 #从链接中获取图片
@@ -41,7 +58,7 @@ def GetImgFromUrl(url):
 
     for n in set(re_Img.findall(pageData)):
         imgfile = n[0]
-        print('正在获取图片：' + n[0])
+        print('正在获取图片：' + imgfile)
         try:
             urllib.request.urlretrieve(imgfile, destFile(imgfile))
         except:
@@ -53,4 +70,4 @@ if __name__ == '__main__':
     # 判断链接是否已经爬过了，用数组判断
     # 多线程爬
     # 判断站内站外的链接， 有些链接时需要 做拼接后才能访问的
-    GetImgFromUrl(mainweburl)
+    GetHrefFromUrl(mainweburl)
