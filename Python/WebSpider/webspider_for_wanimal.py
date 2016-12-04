1861fb4bc00d76d1a20e7e6df375ddbfd2f7e52d
Lights_Jiao <bhjnk945766721@qq.com>
2016-12-04 21:50:53 +0800

    [M]Python: 多线程爬虫for wanimal

--- Python/WebSpider/webspider_for_wanimal.py
+++ Python/WebSpider/webspider_for_wanimal.py
@@ -2,19 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import re
 import os
-import itertools
 from PIL import Image
 from io import BytesIO
-from collections import deque
 from bs4 import BeautifulSoup
+from multiprocessing import pool
 
 # targetDir = "/Users/lights/Desktop/pic" # linux目录
 targetDir = "E:/Picture/Python"  # windows目录
 file_name = "./all_img_url.txt"
 # proxies = {'http': 'http://192.168.100.131:8080'} # 公司的代理
-proxies = {'http': 'http://127.0.0.1:1875'} # lantern代理
+proxies = {'http': 'http://127.0.0.1:1875'}  # lantern代理
+
 
 def dest_file(path):
     if not os.path.isdir(targetDir):
@@ -23,6 +22,7 @@ def dest_file(path):
     pos = path.rindex('/')
     t = os.path.join(targetDir, path[pos + 1:])
     return t
+
 
 def generate_all_page():
     """
@@ -40,28 +40,34 @@ def generate_all_page():
             html_str = requests.get(url=url, proxies=proxies).text
             get_img_url_from_html(page_num, html_str)
         except requests.RequestException:
-            print("获取主页面失败: %s" % (url))
+            print("获取主页面失败: %s" % url)
 
 
 def get_img_url_from_html(page_num, html_str):
     """
     解析html文本并获取图片
     :param page_num:
-    :param url:
+    :param html_str:
     :return:
     """
+    thread_pool = pool.Pool(10)  # 线程池
     soup = BeautifulSoup(html_str, 'html.parser')
     for img in soup.find_all('img'):
         img_url = img.get("src")
-        if img_url != "" or img_url != None:
-            download_img_from_url(page_num, img_url)
+        if img_url != "" or img_url is not None:
+            thread_pool.apply_async(download_img_from_url, args = (page_num, img_url))
+            # download_img_from_url(page_num, img_url)
 
+    print('Waiting for all subprocesses done...')
+    thread_pool.close()
+    thread_pool.join()
+    print('All subprocesses done.')
 
 def download_img_from_url(page_num, img_url):
     """
     从url中下载图片
     :param page_num:
-    :param url:
+    :param img_url:
     :return:
     """
     try:
