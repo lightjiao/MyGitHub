b80b2931719d3b729a9e3cba18bcfa9221b5fd26
lightjiao <lightjiao@easyops.cn>
2017-12-27 00:43:03 +0800

    go study if

--- go/study/study_1_helloworld.go
+++ go/study/study_1_helloworld.go
@@ -1,14 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	fmt.Println(hello())
+	funcIf(1, 2)
 }
 
+func hello() (a string) {
+	return "hello world"
+}
 
-func hello()(a string) {
-    return "hello world"
+func funcIf(first int, cond int) {
+	if first <= 0 {
+		fmt.Printf("first is less than or equal to 0\n")
+	} else if first > 0 && first < 5 {
+		fmt.Printf("> 0 < 5\n")
+	} else if cond == 0 {
+		fmt.Printf("cond")
+	}
+
+	return
 }
