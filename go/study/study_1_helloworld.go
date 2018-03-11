cd7065baf06c24ae589c462b10f52899d4ec2730
lightjiao <lightjiao@easyops.cn>
2018-03-12 00:10:55 +0800

    [M]go循环

--- go/study/study_1_helloworld.go
+++ go/study/study_1_helloworld.go
@@ -35,11 +35,61 @@ func functionReturnError() {
 	}
 
 	fmt.Printf("The integer is %d\n", an)
-	an = an+5
+	an = an + 5
 	newS = strconv.Itoa(an)
 	fmt.Printf("The new string is %s\n", newS)
 }
 
+// 循环体
+func loop() {
+	for i := 0; i < 15; i++ {
+		fmt.Printf("loop: %d\n", i)
+	}
+}
+
+func loop_2() {
+	for i := 0; i < 25; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Printf("G")
+		}
+		fmt.Printf("\n")
+	}
+}
+
+// loop
+func loop_2_2() {
+	str := ""
+	for i := 0; i < 15; i++ {
+		str += "G"
+		fmt.Printf("%s\n", str)
+	}
+}
+
+// switch case && loop
+func loop_3() {
+	for i := 0; i < 100; i++ {
+		switch {
+		case i%15 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
+}
+
+// 死循环
+func loop_4() {
+	i := 10
+	for i > 0 {
+		i = i - 1
+		fmt.Println(i)
+	}
+}
+
 func main() {
-	functionReturnError()
+	loop_4()
 }
--- go/study/study_2.go
+++ go/study/study_2.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "runtime"
+	"fmt"
+	"os"
+	"runtime"
 )
 
 func main() {
-    var goos string = runtime.GOOS
-    fmt.Printf("The operation system is : %s\n", goos)
-    path := os.Getenv("PATH")
-    fmt.Printf("Path is %s \n", path)
+	var goos string = runtime.GOOS
+	fmt.Printf("The operation system is : %s\n", goos)
+	path := os.Getenv("PATH")
+	fmt.Printf("Path is %s \n", path)
 }
