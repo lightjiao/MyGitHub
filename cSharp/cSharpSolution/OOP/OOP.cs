f75b873d4cd3f805cb86f0232cd73d7a76b837c4
light <jhb@gameduchy.com>
2020-07-27 11:50:36 +0800

    :feat: (cSharp): OOP constructor

---
+++ cSharp/cSharpSolution/OOP/OOP.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpSolution.OOP
+{
+    public class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Base print");
+        }
+
+        public virtual void Init()
+        {
+            Console.WriteLine("Base init");
+        }
+    }
+
+    public class OOP : Base
+    {
+        public OOP() : base()
+        {
+            // 这里是先调用 base() 再调用 OOP()
+            Console.WriteLine("OOP print");
+        }
+
+        public override void Init() // 普通函数没有 ` :xxx() `的写法
+        {
+            base.Init();
+        }
+    }
+}
