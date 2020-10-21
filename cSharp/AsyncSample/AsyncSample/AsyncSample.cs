6994786b7ae0dabdd6ddfc828645309397176b79
lightjiao <lightjiao@gmail.com>
2020-10-21 11:20:01 +0800

    add C# async sample

---
+++ cSharp/AsyncSample/AsyncSample/AsyncSample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncSample
+{
+    internal class AsyncSample
+    {
+        public static async Task DoSomething(int num)
+        {
+            Console.WriteLine($"DoAsync {num} start");
+
+            await SleepAsync();
+
+            Console.WriteLine($"DoAsync {num} end");
+        }
+
+        private static async Task SleepAsync()
+        {
+            await Task.Run(() => { Thread.Sleep(2000); });
+        }
+    }
+}
---
+++ cSharp/AsyncSample/AsyncSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task.WaitAll(AsyncSample.DoSomething(1));
+        }
+    }
+}
