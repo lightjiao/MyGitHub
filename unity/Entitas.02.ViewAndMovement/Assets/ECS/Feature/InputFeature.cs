57542125c28025325b60f1876a49d4c8339b2fb5
lightjiao <lightjiao@gmail.com>
2021-01-11 23:45:21 +0800

    (unity)(ECS):输入移动系统

---
+++ unity/Entitas.02.ViewAndMovement/Assets/ECS/Feature/InputFeature.cs
@@ -0,0 +1,9 @@
+﻿public class InputFeature : Feature
+{
+    public InputFeature(Contexts contexts) : base("Input Systems")
+    {
+        Add(new EmitInputSystem(contexts));
+        Add(new CreateMoverSystem(contexts));
+        Add(new CommandMoveSystem(contexts));
+    }
+}
---
+++ unity/Entitas.02.ViewAndMovement/Assets/ECS/System/CommandMoveSystem.cs
@@ -0,0 +1,34 @@
+﻿using Entitas;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandMoveSystem : ReactiveSystem<InputEntity>
+{
+    private readonly GameContext _gameContext;
+    private readonly IGroup<GameEntity> _movers;
+
+    public CommandMoveSystem(Contexts contexts) : base(contexts.input)
+    {
+        _movers = contexts.game.GetGroup(GameMatcher.AllOf(GameMatcher.Mover).NoneOf(GameMatcher.Move));
+    }
+
+    protected override ICollector<InputEntity> GetTrigger(IContext<InputEntity> context)
+    {
+        return context.CreateCollector(InputMatcher.AllOf(InputMatcher.LeftMouse, InputMatcher.MouseDown));
+    }
+
+    protected override bool Filter(InputEntity entity)
+    {
+        return entity.hasMouseDown;
+    }
+
+    protected override void Execute(List<InputEntity> entities)
+    {
+        foreach (InputEntity e in entities)
+        {
+            GameEntity[] movers = _movers.GetEntities();
+            if (movers.Length <= 0) return;
+            movers[Random.Range(0, movers.Length)].ReplaceMove(e.mouseDown.position);
+        }
+    }
+}
