46da29459a4a9f67cfdd203687da642e9e82c36b
lightjiao <lightjiao@gmail.com>
2021-01-12 00:03:00 +0800

    (unity)(ECS):ECS实现小蜜蜂随着鼠标点击移动完成

---
+++ unity/Entitas.02.ViewAndMovement/Assets/ECS/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private Systems _systems;
+    private Contexts _contexts;
+
+    private void Start()
+    {
+        _contexts = Contexts.sharedInstance;
+        _systems = CreateSystems(_contexts);
+
+        _systems.Initialize();
+    }
+
+    private void Update()
+    {
+        _systems.Execute();
+        _systems.Cleanup();
+    }
+
+    private static Systems CreateSystems(Contexts contexts)
+    {
+        return new Feature("Systems")
+            .Add(new InputFeature(contexts))
+            .Add(new MovementFeature(contexts))
+            .Add(new ViewFeature(contexts));
+    }
+}
