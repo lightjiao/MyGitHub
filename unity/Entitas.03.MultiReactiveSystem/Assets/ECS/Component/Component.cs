acc4837b5ee5c0eef4666b39df9d4e6d3994719e
lightjiao <lightjiao@gmail.com>
2021-01-16 13:37:53 +0800

    (unity)(ECS)MultiReactiveSystem简单的销毁系统

---
+++ unity/Entitas.03.MultiReactiveSystem/Assets/ECS/Component/Component.cs
@@ -0,0 +1,11 @@
+﻿using Entitas;
+using UnityEngine;
+
+/// <summary>
+/// 一个flag component，用于标识是否销毁
+/// </summary>
+[Game, Input, Ui]
+public class DestroyedComponent : IComponent
+{
+}
+
---
+++ unity/Entitas.03.MultiReactiveSystem/Assets/ECS/System/MultiDestroySystem.cs
@@ -0,0 +1,45 @@
+﻿using Entitas;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 表示是一个有 DestroyedComponent 的entity
+/// </summary>
+public interface IDestroyableEntity : IEntity, IDestroyedEntity { }
+
+// 告诉编译器，我们指定的这几个entity都实现了 IDestroyableEntity
+public partial class GameEntity : IDestroyableEntity { }
+
+public partial class InputEntity : IDestroyableEntity { }
+
+public partial class UiEntity : IDestroyableEntity { }
+
+public class MultiDestroySystem : MultiReactiveSystem<IDestroyableEntity, Contexts>
+{
+    public MultiDestroySystem(Contexts contexts) : base(contexts)
+    {
+    }
+
+    protected override ICollector[] GetTrigger(Contexts contexts)
+    {
+        return new ICollector[] {
+            contexts.game.CreateCollector(GameMatcher.Destroyed),
+            contexts.input.CreateCollector(InputMatcher.Destroyed),
+            contexts.ui.CreateCollector(UiMatcher.Destroyed),
+        };
+    }
+
+    protected override bool Filter(IDestroyableEntity entity)
+    {
+        return entity.isDestroyed;
+    }
+
+    protected override void Execute(List<IDestroyableEntity> entities)
+    {
+        foreach (var e in entities)
+        {
+            Debug.Log($"Destroyed Entity from {e.contextInfo.name} context");
+            e.Destroy();
+        }
+    }
+}
