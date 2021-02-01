522f5a411bfc85c5d53750e9e19e7e488a3df7e8
lightjiao <lightjiao@gmail.com>
2021-02-01 18:55:30 +0800

    (C#):提交测试代码

--- cSharp/cSharpSolution/Program.cs
+++ cSharp/cSharpSolution/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 namespace cSharpSolution
 {
@@ -10,11 +10,26 @@ namespace cSharpSolution
 
         private static void Main(string[] args)
         {
-            s_httpClient.GetStringAsync("http://www.baidu.com").ContinueWith(
-                (downloadTask) =>
-                {
-                    Console.WriteLine(downloadTask.Result);
-                }, TaskScheduler.FromCurrentSynchronizationContext());
+            var testMap = new Dictionary<int, HashSet<int>>();
+            testMap[1] = new HashSet<int> { 1, 1, 1, 1 };
+
+            Console.WriteLine(testMap[1]);
+
+            /// 字典的 ?[index] 会报错
+            /// System.Collections.Generic.HashSet`1[System.Int32]
+            /// Unhandled exception. System.Collections.Generic.KeyNotFoundException: The given key '2' was not present in the dictionary.
+            /// at System.Collections.Generic.Dictionary`2.get_Item(TKey key)
+            /// at cSharpSolution.Program.Main(String[] args) in C:\develop\MyGitHub\cSharp\cSharpSolution\Program.cs:line 19
+            //var c = testMap?[2];
+            //Console.WriteLine(c);
+
+            HashSet<int> ret = new HashSet<int>()
+                ;
+            var tryRet = testMap?.TryGetValue(1, out ret);
+            Console.WriteLine(tryRet);
+
+            tryRet = testMap?.TryGetValue(2, out ret);
+            Console.WriteLine(tryRet);
         }
 
         private static async void Main2(string[] args)
