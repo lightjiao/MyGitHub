21cee05bd24cf93c348e9b83112e2faf59685cc7
lightjiao <lightjiao@gmail.com>
2021-03-07 17:57:07 +0800

    (C#):用await优雅的实现异步逻辑

--- cSharp/WindowsFormsAppSample/WindowsFormsAppSample/FormSample.cs
+++ cSharp/WindowsFormsAppSample/WindowsFormsAppSample/FormSample.cs
@@ -14,17 +14,22 @@ namespace WindowsFormsAppSample
 
         private static readonly HttpClient s_httpClient = new HttpClient();
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
-            s_httpClient.GetStringAsync("http://www.baidu.com").ContinueWith(
-                downloadTask =>
-                {
-                    button1.Text = downloadTask.Result;
-                },
-                // 如果不指定contenxt的话，的确会报
-                // System.InvalidOperationException: 'The calling thread cannot access this object because a different thread owns it.'
-                TaskScheduler.FromCurrentSynchronizationContext() 
-            );
+            //s_httpClient.GetStringAsync("http://www.baidu.com").ContinueWith(
+            //    downloadTask =>
+            //    {
+            //        button1.Text = downloadTask.Result;
+            //    },
+            //    // 如果不指定contenxt的话，的确会报
+            //    // System.InvalidOperationException: 'The calling thread cannot access this object because a different thread owns it.'
+            //    TaskScheduler.FromCurrentSynchronizationContext() 
+            //);
+
+            // 直接使用await来优雅的实现
+            // 但如果加了configureAwait(false)，反而会报错，原因和上面一样，执行到了完全不同的线程去了
+            var text = await s_httpClient.GetStringAsync("http://www.baidu.com");// .ConfigureAwait(false);
+            button1.Text = text;
         }
     }
 }
