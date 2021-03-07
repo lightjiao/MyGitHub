9dc8094ae65300f618ed6708d4953594b1fc31cb
lightjiao <lightjiao@gmail.com>
2021-03-07 17:33:35 +0800

    (C#):点击按钮异步的展示内容到Button

--- cSharp/WindowsFormsAppSample/WindowsFormsAppSample/FormSample.cs
+++ cSharp/WindowsFormsAppSample/WindowsFormsAppSample/FormSample.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,9 +12,19 @@ namespace WindowsFormsAppSample
             InitializeComponent();
         }
 
+        private static readonly HttpClient s_httpClient = new HttpClient();
+
         private void button1_Click(object sender, EventArgs e)
         {
-
+            s_httpClient.GetStringAsync("http://www.baidu.com").ContinueWith(
+                downloadTask =>
+                {
+                    button1.Text = downloadTask.Result;
+                },
+                // 如果不指定contenxt的话，的确会报
+                // System.InvalidOperationException: 'The calling thread cannot access this object because a different thread owns it.'
+                TaskScheduler.FromCurrentSynchronizationContext() 
+            );
         }
     }
 }
