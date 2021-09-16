0c9196aaf93b1037fd48667387b52f5bb4d94b46
lightjiao <lightjiao@gmail.com>
2021-09-16 19:32:19 +0800

    (unity): Mesh UV 入门

---
+++ unity/sample/Assets/SimpleSample/MeshUV/GridMesh.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// 自定义一个网格的Mesh
+/// </summary>
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class GridMesh : MonoBehaviour
+{
+    public int xSize;
+    public int ySize;
+    
+    
+}
---
+++ unity/sample/Assets/SimpleSample/MeshUV/MeshUVAdjuster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshUVAdjuster : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+
+    private Mesh mCacheMesh;
+
+    private Vector2[] mCacheUVArray;
+
+    private void OnEnable()
+    {
+        MeshScaleCache();
+    }
+
+    private void Update()
+    {
+        MeshScaleUpdate();
+    }
+
+    // 缓存初始UV
+    private void MeshScaleCache()
+    {
+        mCacheMesh = meshFilter.mesh;
+
+        var meshUV = mCacheMesh.uv;
+        mCacheUVArray = new Vector2[meshUV.Length];
+        for (var i = 0; i < meshUV.Length; i++)
+        {
+            mCacheUVArray[i] = meshUV[i];
+        }
+    }
+
+    private void MeshScaleUpdate()
+    {
+        const float MAX_X_SCALE = 5f;
+
+        var scaleRatio = transform.lossyScale.x / MAX_X_SCALE;
+        var uv = mCacheMesh.uv;
+        for (var i = 0; i < uv.Length; i++)
+        {
+            uv[i].x = mCacheUVArray[i].x * scaleRatio;
+        }
+
+        mCacheMesh.uv = uv;
+    }
+}
