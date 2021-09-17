24f153320a8e8ce963cba65e750a63248e08c274
lightjiao <lightjiao@gmail.com>
2021-09-17 11:21:08 +0800

    (unity): 生成grid的点

--- unity/sample/Assets/SimpleSample/MeshUV/GridMesh.cs
+++ unity/sample/Assets/SimpleSample/MeshUV/GridMesh.cs
@@ -7,8 +7,54 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class GridMesh : MonoBehaviour
 {
-    public int xSize;
-    public int ySize;
-    
-    
+    public int xSize = 10;
+    public int ySize = 5;
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    private void Awake()
+    {
+        Generate();
+    }
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+            }
+        }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+
+        mesh.triangles = triangles;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+        foreach (var vec in vertices)
+        {
+            Gizmos.DrawSphere(vec + transform.position, 0.1f);
+        }
+    }
 }
