fe7cf9916fc93fcb597f36295ec3aa24f1c35f38
lightjiao <lightjiao@gmail.com>
2021-10-22 01:42:10 +0800

    (unity): GF加载UI

---
+++ unity/GFBenmutouTutorial/Assets/Demo3/Demo3_ProcedureLaunch.cs
@@ -0,0 +1,17 @@
+﻿using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class Demo3_ProcedureLaunch : ProcedureBase
+{
+    protected override void OnEnter(ProcedureOwner procedureOwner)
+    {
+        base.OnEnter(procedureOwner);
+
+        var scene = GameEntry.GetComponent<SceneComponent>();
+        scene.LoadScene("Assets/Demo3/Demo3_Menu.unity", this);
+        // scene.LoadScene("Demo3_Menu", this);
+
+        ChangeState<Demo3_ProcedureMenu>(procedureOwner);
+    }
+}
---
+++ unity/GFBenmutouTutorial/Assets/Demo3/Demo3_ProcedureMenu.cs
@@ -0,0 +1,15 @@
+﻿using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class Demo3_ProcedureMenu : ProcedureBase
+{
+    protected override void OnEnter(ProcedureOwner procedureOwner)
+    {
+        base.OnEnter(procedureOwner);
+
+        var ui = GameEntry.GetComponent<UIComponent>();
+
+        ui.OpenUIForm("Assets/Demo3/UI_Menu.prefab", "DefaultGroup");
+    }
+}
