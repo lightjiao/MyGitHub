d5bb8101f5e445562331bf14e4bd1a4354c76434
lightjiao <lightjiao@gmail.com>
2021-10-22 00:55:58 +0800

    (unity): Demo2 加载场景

---
+++ unity/GFBenmutouTutorial/Assets/Demo2/Demo2_ProcedureMenu.cs
@@ -0,0 +1,13 @@
+﻿using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+public class Demo2_ProcedureMenu : ProcedureBase
+{
+    protected override void OnEnter(ProcedureOwner procedureOwner)
+    {
+        base.OnEnter(procedureOwner);
+
+        Log.Info("可以在这里加载菜单");
+    }
+}
