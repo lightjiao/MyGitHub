8ac23d231513eb3517d47a392886cc7fe1b53385
lightjiao <lightjiao@gmail.com>
2021-10-24 20:58:01 +0800

    (unity): GF 网络请求

--- unity/GFBenmutouTutorial/Assets/Demo3/Demo3_ProcedureMenu.cs
+++ unity/GFBenmutouTutorial/Assets/Demo3/Demo3_ProcedureMenu.cs
@@ -1,4 +1,5 @@
-﻿using GameFramework.Event;
+﻿using GameFramework;
+using GameFramework.Event;
 using GameFramework.Procedure;
 using UnityGameFramework.Runtime;
 using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
@@ -16,7 +17,7 @@ public class Demo3_ProcedureMenu : ProcedureBase
         ui.OpenUIForm("Assets/Demo3/UI_Menu.prefab", "DefaultGroup", this);
 
         Log.Info("ProcedureMenu OnEnter last");
-        
+
         /*
          * 预期的 async/await 写法：
          * var result = await ui.OpenUIForm()
@@ -28,6 +29,16 @@ public class Demo3_ProcedureMenu : ProcedureBase
 
         var entity = GameEntry.GetComponent<EntityComponent>();
         entity.ShowEntity<HeroLogic>(1, "Assets/Demo3/HeroCube.prefab", "EntityGroup");
+
+        gfEvent.Subscribe(WebRequestSuccessEventArgs.EventId, WebRequestSuccess);
+        var webRequest = GameEntry.GetComponent<WebRequestComponent>();
+        webRequest.AddWebRequest("https://www.baidu.com", this);
+    }
+
+    private void WebRequestSuccess(object sender, GameEventArgs e)
+    {
+        var ne = (WebRequestSuccessEventArgs) e;
+        Log.Info("Response: " + Utility.Converter.GetString(ne.GetWebResponseBytes()));
     }
 
     private void OnOpenUIFormSuccess(object sender, GameEventArgs e)
