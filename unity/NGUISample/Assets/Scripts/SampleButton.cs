8607b070c9599217daab01fed728a5fc5205533b
lightjiao <lightjiao@gmail.com>
2022-06-20 19:30:40 +0800

    NGUI Toturial 2

---
+++ unity/NGUISample/Assets/Scripts/SampleButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleButton : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("OnButton Click");
+    }
+}
